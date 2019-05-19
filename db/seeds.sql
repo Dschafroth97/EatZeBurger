fc64c127900c3a0f9dbf0a3d07e03bec2e235364
Donovan Schafroth <donovanschafroth@gmail.com>
2019-05-19 07:53:27 -0500

    Repo transfer

---
+++ db/seeds.sql
@@ -0,0 +1,5 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Red Lentil Cauliflower Burger w/ Chipotle Habanero Mayo', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoky Amaranth Black Bean Burger', true);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Black Lentil Pistachio Shiitake Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Smoky BBQ Sweet Potato Burger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('BBQ Lentil Burger w/ Mango Carrot Slaw', true);
